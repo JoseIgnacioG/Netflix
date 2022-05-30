930dce44e2144ce9f35f67aeb28b9abffa734080
Alejandro Garcia Villar <alejandro.garcia-villar@gft.com>
2022-05-30 19:46:19 +0200

    Creación Proyecto Netflix

---
+++ src/test/java/com/example/netflixbueno/NetflixBuenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netflixbueno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixBuenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
